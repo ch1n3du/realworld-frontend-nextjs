0b089c69bba5e669bcc6f59f18c1063b8e6ae22c
Ch1n3du <danielonyesoh@gmail.com>
2023-06-23 15:13:03 +0100

    chore: Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'md': '45rem',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
